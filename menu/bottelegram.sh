aa2648b1a759cb8ec758713b52e1189de2e7f6f1
yusuf2010847 <stokmltaufik5@gmail.com>
2025-05-01 19:14:35 +0700

    Add files via upload

---
+++ menu/bottelegram.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+colornow=$(cat /etc/rmbl/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m"
+COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+ipsaya=$(curl ifconfig.me)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/yusuf2010847/permission/main/ip"
+checking_sc() {
+useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
+if [[ $date_list < $useexp ]]; then
+echo -ne
+else
+systemctl stop nginx
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│${NC}${COLBG1}          ${WH}• AUTOSCRIPT PREMIUM •                 ${NC}$COLOR1│ $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│            ${RED}PERMISSION DENIED !${NC}                  $COLOR1│"
+echo -e "$COLOR1│   ${yl}Your VPS${NC} $ipsaya \033[0;36mHas been Banned${NC}      $COLOR1│"
+echo -e "$COLOR1│     ${yl}Buy access permissions for scripts${NC}          $COLOR1│"
+echo -e "$COLOR1│             \033[0;32mContact Your Admin ${NC}                 $COLOR1│"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+exit
+fi
+}
+checking_sc
+IP=$(curl -sS ipv4.icanhazip.com);
+date=$(date +"%Y-%m-%d")
+time=$(date +'%H:%M:%S')
+domain=$(cat /etc/xray/domain)
+token=$(cat /usr/bin/token)
+id_chat=$(cat /usr/bin/idchat)
+clear
+sleep 1
+echo -e "${COLOR1}[ INFO ] ${WH}Processing... "
+mkdir -p /root/backup
+echo -e "${COLOR1}[ INFO ] ${WH}Mohon Ditunggu... "
+wget -O /root/backup/ipmini https://raw.githubusercontent.com/yusuf2010847/permission/main/ip &> /dev/null
+cp -r /etc/passwd /root/backup/ &> /dev/null
+cp -r /etc/group /root/backup/ &> /dev/null
+cp -r /etc/shadow /root/backup/ &> /dev/null
+cp -r /etc/gshadow /root/backup/ &> /dev/null
+cp -r /usr/bin/idchat /root/backup/ &> /dev/null
+cp -r /usr/bin/token /root/backup/ &> /dev/null
+cp -r /etc/per/id /root/backup/ &> /dev/null
+cp -r /etc/per/token /root/backup/token2 &> /dev/null
+cp -r /etc/perlogin/id /root/backup/loginid &> /dev/null
+cp -r /etc/perlogin/token /root/backup/logintoken &> /dev/null
+cp -r /etc/xray/config.json /root/backup/xray &> /dev/null
+cp -r /etc/xray/ssh /root/backup/ssh &> /dev/null
+cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
+cp -r /etc/xray/sshx /root/backup/sshx &> /dev/null
+cp -r /etc/vmess /root/backup/vmess &> /dev/null
+cp -r /etc/vless /root/backup/vless &> /dev/null
+cp -r /etc/trojan /root/backup/trojan &> /dev/null
+cp -r /etc/issue.net /root/backup/issue &> /dev/null
+cd /root
+zip -r $IP.zip backup > /dev/null 2>&1
+zip -r $IP-$date.zip backup > /dev/null 2>&1
+rclone copy /root/$IP-$date.zip dr:backup/
+url=$(rclone link dr:backup/$IP-$date.zip)
+id=(`echo $url | grep '^https' | cut -d'=' -f2`)
+link="https://drive.google.com/u/4/uc?id=${id}&export=download"
+curl -F chat_id="$id_chat" -F document=@"$IP.zip" -F caption="Thank You For Using this Script
+Domain : $domain
+IP VPS : $IP
+Date   : $date
+Time   : $time WIB
+Link Google : $link" https://api.telegram.org/bot$token/sendDocument &> /dev/null
+rm -fr /root/backup &> /dev/null
+rm -fr /root/user-backup &> /dev/null
+rm -f /root/$NameUser.zip &> /dev/null
+rm -r /root/$IP-$date.zip &> /dev/null
+rm -f /root/$IP.zip &> /dev/null
+echo " Please Check Your BOT"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
